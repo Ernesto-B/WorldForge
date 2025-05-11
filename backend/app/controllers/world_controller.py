2445e294cda616be8012ddd3123e3f1d72e33a18
JReeseSanchez <jaiden.sanchez@ucalgary.ca>
2025-05-11 15:06:26 -0600

    !feat: Update World Settings
    Incomplete functionality to change the settings.

--- backend/app/controllers/world_controller.py
+++ backend/app/controllers/world_controller.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-from pydantic import BaseModel, EmailStr
-from pydantic_extra_types.pendulum_dt import Date
+from typing import Any
+from pydantic import BaseModel, EmailStr, Json
 from app.services.create_world import create_world
 from app.services.search_world_id import search_world_id
+from app.services.update_world_settings import change_settings
 from app.db.supabaseDB import get_db
 from app.db.models import World
 from app.core.security import get_current_user_id
@@ -16,6 +17,21 @@ class newWorld(BaseModel):
 
 class world_search_id(BaseModel):
     search: int
+
+class settings(BaseModel):
+    allow_public_visibility: bool
+    join_method: str
+    max_campaigns: int
+    allow_dm_invites: bool
+    enable_fog_of_war: bool
+    inter_party_visibility: bool
+    spectator_map_visibility: bool
+    show_party_position_globally: bool
+
+class new_world_settings(BaseModel):
+    settings_id: int
+    changed_settings: settings
+
 
 @world_controller.post("/create_world")
 def new_world(
@@ -36,4 +52,12 @@ def get_world_by_id(
 
 # = = = = = = = = = = = = = = = = = 
 # World Settings
+
+@world_controller.post("/update_world_settings")
+def update_world_settings(
+    request: new_world_settings,
+    db: Session = Depends(get_db)
+):
+    data = change_settings(request.settings_id, request.changed_settings, db)    
+    return data
 
--- backend/app/services/search_world_id.py
+++ backend/app/services/search_world_id.py
@@ -4,7 +4,6 @@ from app.core.exceptions import BadRequestError, DatabaseSaveError
 def search_world_id(search_id:int, db):
     try:
         result = db.query(World).filter(World.id == search_id).first()
-
         return result
     
     except Exception as e: 
